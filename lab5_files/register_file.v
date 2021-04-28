3775734d51dc8144e3cea7ed09f407ace1d5647d
21hak <2wonakk@gmail.com>
2021-04-29 00:31:34 +0900

    latches, alu, register_file, util

--- lab5_files/register_file.v
+++ lab5_files/register_file.v
@@ -14,5 +14,20 @@ module register_file (read_out1, read_out2, read1, read2, dest, write_data, reg_
 	output [`WORD_SIZE-1:0] read_out2;
 	
 	//TODO: implement register file
+	reg [`WORD_SIZE-1:0] registers [3:0];
 
+	initial begin
+		registers[0] = 0;
+		registers[1] = 0;
+		registers[2] = 0;
+		registers[3] = 0;
+	end
+	assign read_out1 = registers[read1];
+	assign read_out2 = registers[read2];
+    
+	always @(posedge clk) begin
+		if(reg_write) begin
+				registers[dest] <= write_data;    
+		end
+	end
 endmodule
