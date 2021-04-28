f31c1aa676cb15f554f34695541b6da7b507a009
21hak <2wonakk@gmail.com>
2021-04-29 01:08:45 +0900

    add pc

---
+++ lab5_files/pc.v
@@ -0,0 +1,24 @@
+`include "opcodes.v" 	   
+
+module PC(pc_in, pc_out, reset_n, clk);
+	input clk;
+	input reset_n;
+	input [`WORD_SIZE-1:0] pc_in;
+	output reg [`WORD_SIZE-1:0] pc_out;
+
+	initial begin
+		pc_out = 0;
+	end
+	
+	always @(posedge reset_n)
+	begin
+		pc_out <= 0 ;
+	end
+	
+	always @(posedge clk) 
+	begin
+        pc_out <= pc_in;    
+	end
+endmodule
+
+				
