ef50e596f176dd73c7b2887a1a62119a22d6cd54
keechang <rlckd159g@gmail.com>
2021-05-22 01:59:40 +0900

    cache_frame

--- lab6_files/lab6_cache/cache.v
+++ lab6_files/lab6_cache/cache.v
@@ -18,18 +18,71 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 
 	
 	reg [4*`WORD_SIZE-1:0] cache_lines[3:0];
-
+	reg [`WORD_SIZE-1-4:0] tag_lines[3:0];
 	reg [3:0] valid;
 	reg [3:0] dirty;
+
 
 	wire [1:0] index;
 	wire [`WORD_SIZE-1-4:0] tag;
 	wire [1:0] bo;
 	
+	wire hit;
+	
+	
 	assign bo = address_c[1:0];
 	assign index = address_c[3:2];
 	assign tag = address_c[`WORD_SIZE-1:4];
 	
-		
+	
+	assign hit = valid[index] && tag_lines[index];
+
+	always @(posedge clk) begin
+		if(!reset_n) begin
+			cache_lines[0] <= 0;
+			cache_lines[1] <= 0;
+			cache_lines[2] <= 0;
+			cache_lines[3] <= 0;
+
+			tag_lines[0] <= 0;
+			tag_lines[1] <= 0;
+			tag_lines[2] <= 0;
+			tag_lines[3] <= 0;
+
+			valid[0] <= 0;
+			valid[1] <= 0;
+			valid[2] <= 0;
+			valid[3] <= 0;
+
+			dirty[0] <= 0;
+			dirty[1] <= 0;
+			dirty[2] <= 0;
+			dirty[3] <= 0;
+		end
+	end
+
+	always @(posedge clk) begin
+		if(ready_c) begin
+			num_access <= num_access + 1;
+		end
+		if(write_c) begin
+			if (hit) begin
+			end
+			if (!hit) begin
+			end
+		end
+		if(read_c) begin
+			if (hit) begin
+			end
+			if (!hit) begin
+			end
+		end
+	end
+	always @(*) begin
+		read_c
+		write_c
+		ready_c
+
+	end
 
 end module
