e813c2bb473fd78c1e3dc6622600dad97396933d
keechang <rlckd159g@gmail.com>
2021-05-24 01:31:49 +0900

    inst_cache_all

--- lab6_files/lab6_cache/cache.v
+++ lab6_files/lab6_cache/cache.v
@@ -1,4 +1,6 @@
+`timescale 1ns/1ns
 `define WORD_SIZE 16
+
 
 module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m, address_m, read_m, write_m, ready_c, num_access, num_hit);
 	input clk;
@@ -9,15 +11,16 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 	
 	input [`WORD_SIZE-1:0] address_c;
 
-	output [`WORD_SIZE-1:0] data_c;
-	reg [`WORD_SIZE-1:0] data_c;
+	//output [`WORD_SIZE-1:0] data_c;
+	//reg [`WORD_SIZE-1:0] data_c;
+	
+	
+	inout [`WORD_SIZE-1:0] data_c;
 	inout [`WORD_SIZE * 4 -1:0] data_m;
 
 	output [`WORD_SIZE-1:0] address_m;
-	reg [`WORD_SIZE-1:0] address_m;
-	output read_m;
-	reg read_m;
-	output write_m;
+	output reg read_m;
+	output reg write_m;
 	output reg ready_c;
 	output reg [`WORD_SIZE-1:0] num_access;
 	output reg [`WORD_SIZE-1:0] num_hit;
@@ -29,6 +32,7 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 	reg [3:0] dirty;
 
 
+
 	wire [1:0] index;
 	wire [`WORD_SIZE-1-4:0] tag;
 	wire [1:0] bo;
@@ -36,27 +40,28 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 	wire hit;
 	
 	
+
 	assign bo = address_c[1:0];
 	assign index = address_c[3:2];
 	assign tag = address_c[`WORD_SIZE-1:4];
 	
-	
+	assign address_m = address_c;
+	assign data_c = ((read_c && ready_c) ? cache_lines[index][`WORD_SIZE * unsigned'(bo) +: `WORD_SIZE ] : {`WORD_SIZE{1'bz}});
 	// assign hit = valid[index] && tag_lines[index];
 	initial begin
 		ready_c = 0;
 		num_hit = 0;
 		num_access = 0;
-
 	end
 
 	always @(posedge clk) begin
 		if(!reset_n) begin
-			data_c <= 0;
+			//data_c <= 0;
 			num_hit <= 0;
 			num_hit <= 0;
 			ready_c <= 0;
 			read_m <= 0;
-			address_m <= 0;
+			//address_m <= 0;
 			cache_lines[0] <= 0;
 			cache_lines[1] <= 0;
 			cache_lines[2] <= 0;
@@ -76,6 +81,7 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 			dirty[1] <= 0;
 			dirty[2] <= 0;
 			dirty[3] <= 0;
+			
 		end
 	end
 
@@ -95,39 +101,30 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 		end
 		if(read_c) begin
 			num_access <= num_access + 1;
-			if (hit) begin
-				$display("###### read hit");
-				data_c <= cache_lines[index][`WORD_SIZE * unsigned'(bo) +: `WORD_SIZE];
-				ready_c <= 1 ;
-				num_hit <= num_hit + 1;
-				read_m <= 0;
+			if(ready_m) begin
+				valid[index] <= 1;
+				cache_lines[index][`WORD_SIZE*0 +: `WORD_SIZE] <= data_m[`WORD_SIZE*0 + : `WORD_SIZE];
+				cache_lines[index][`WORD_SIZE*1 +: `WORD_SIZE] <= data_m[`WORD_SIZE*1 + : `WORD_SIZE];
+				cache_lines[index][`WORD_SIZE*2 +: `WORD_SIZE] <= data_m[`WORD_SIZE*2 + : `WORD_SIZE];
+				cache_lines[index][`WORD_SIZE*3 +: `WORD_SIZE] <= data_m[`WORD_SIZE*3 + : `WORD_SIZE];
+				tag_lines[index] <= tag;
+				//data_c <= data_m[`WORD_SIZE * unsigned'(bo) +: `WORD_SIZE];
+					//ready_c <= 1;
+					//read_m <= 0;
 			end
-			else begin
-				$display("###### read miss");
-				if(ready_m) begin
-					valid[index] <= 1;
-					cache_lines[index][`WORD_SIZE*0 +: `WORD_SIZE] <= data_m[`WORD_SIZE*0 + : `WORD_SIZE];
-					cache_lines[index][`WORD_SIZE*1 +: `WORD_SIZE] <= data_m[`WORD_SIZE*1 + : `WORD_SIZE];
-					cache_lines[index][`WORD_SIZE*2 +: `WORD_SIZE] <= data_m[`WORD_SIZE*2 + : `WORD_SIZE];
-					cache_lines[index][`WORD_SIZE*3 +: `WORD_SIZE] <= data_m[`WORD_SIZE*3 + : `WORD_SIZE];
-					tag_lines[index] <= tag;
-					data_c <= data_m[`WORD_SIZE * unsigned'(bo) +: `WORD_SIZE];
-					ready_c <= 1;
-					read_m <= 0;
-				end
-				else begin
-					read_m <= 1;
-					address_m <= address_c;
-					ready_c <= 0;
-				end
-			end
-		end
-		else begin
-			ready_c <= 0;
 		end
 		
 	end
+	always @(*) begin
+		if(reset_n) begin
+			
+		end
+		read_m = (read_c && !hit);
+		write_m = 0;
+		ready_c = read_c && hit;
+	end
 	always @(posedge clk ) begin
+		#(10);
 		$display("#################");
 		$display("##### address_c: %b", address_c);
 		$display("##### hit: 		%b", hit);
@@ -148,5 +145,7 @@ module cache (clk, reset_n, read_c, write_c, ready_m, address_c, data_c, data_m,
 	// 	ready_c
 
 	// end
+
+
 
 endmodule
